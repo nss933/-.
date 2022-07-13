b8b110f57b4bf3cf58b75e5e51429e78c33d70c3
nss <2497279669@qq.com>
2022-07-13 19:56:54 +0800

    下载页面

---
+++ vue-project/src/views/Download.vue
@@ -0,0 +1,525 @@
+<template>
+  <div class="download">
+    <div class="title">
+      <div class="center">
+        <h1>下载</h1>
+      </div>
+    </div>
+
+    <div class="app">
+      <div class="center">
+        <h4>App应用程序</h4>
+        <div class="line"></div>
+        <div class="log-model">
+          <img src="../assets/download/dj.png" alt="" />
+          <a class="right">
+            <div class="r1">DJI Fly</div>
+            <a class="r2"
+              >全新航拍界面简洁美观，便捷易用。支持 御 Mavic Mini、御 Mavic Air
+              2、DJI Mini 2、DJI FPV、DJI Air 2S、DJI Mini SE、DJI Mavic 3、DJI
+              Mini 3 Pro</a
+            >
+          </a>
+        </div>
+        <div class="line"></div>
+        <ul>
+          <li>
+            <img src="../assets/download/ios.png" alt="" />
+            <div>iOS 版本 V 1.6.8</div>
+            <div>需要 iOS 11.0 或更高版本。</div>
+            <a>查看推荐机型</a>
+          </li>
+          <li>
+            <img src="../assets/download/android.png" alt="" />
+            <p>Android 版本 V 1.6.8</p>
+            <p>需要 Android 6.0 或更高版本。</p>
+            <a>查看推荐机型</a>
+          </li>
+          <li>
+            <img src="../assets/download/1.jpg" style="width: 120px" alt="" />
+            <!-- <p>iOS 版本 V 1.6.8</p>
+            <p>需要 iOS 11.0 或更高版本。</p>
+            <a>查看推荐机型</a> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="software">
+      <div class="center">
+        <h3 class="title">软件与驱动</h3>
+        <div class="line"></div>
+        <div class="log-model">
+          <img src="../assets/download/1.png" alt="" />
+          <a class="right">
+            <div class="r1">DJI Assistant 2 (消费机系列)</div>
+            <a class="r2">支持消费无人机系列产品，包括Mavic系列。</a>
+          </a>
+        </div>
+        <div class="line"></div>
+        <ul class="compute">
+          <li class="mac">
+            <img src="../assets/download/2.png" alt="" />
+            <div class="com-b">
+              <div>Mac 版本 V2.1.10</div>
+              <div>2021-05-11</div>
+              <a>
+                <span>pkg</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </div>
+          </li>
+          <li class="window">
+            <img src="../assets/download/3.png" alt="" />
+            <div class="com-b">
+              <div>Windows 版本 V2.1.10</div>
+              <div>2022-05-11</div>
+              <a>
+                <p>pkg</p>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </div>
+          </li>
+        </ul>
+        <div class="line"></div>
+        <div class="down" style="width:1100px; padding-right:0;">
+          <ul>
+            <li class="li1">
+              <p>DJI Assistant 2 (消费机系列) 发布记录 V2.1.10</p>
+              <p>2022-05-11</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+       
+      </div>
+    </div>
+
+    <div class="tab">
+      <h4>文档</h4>
+      <div class="line"></div>
+      <div class="down" >
+          <ul>
+            <li class="li1">
+              <p>DJI Mini 3 Pro - 用户手册 v1.2</p>
+              <p>2022-06-30</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      <div class="line"></div>
+       <!-- 第二个 -->
+  <div class="down down2">
+          <ul>
+            <li class="li1">
+              <p>DDJI Mini 3 Pro - 快速入门指南 v1.0</p>
+              <p>2022-05-10</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- 第三个 -->
+        <div class="line"> </div>
+<div class="down">
+          <ul>
+            <li class="li1">
+              <p>DJI Mini 3 Pro - 快速入门指南 (带屏遥控器版) v1.0</p>
+              <p>2022-05-10</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="line"></div>
+        <!-- 第四个 -->
+      <div class="down down2">
+          <ul>
+            <li class="li1">
+              <p>DJI Mini 3 Pro - 快速入门指南 (单机) v1.0</p>
+              <p>2022-05-10</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- 第三个 -->
+        <div class="line"> </div>
+<div class="down">
+          <ul>
+            <li class="li1">
+              <p>DDJI Mini 3 Pro - 安全概要 v1.0</p>
+              <p>2022-05-11</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="line"></div>
+        <!-- 接下去 -->
+    <div class="down down2">
+          <ul>
+            <li class="li1">
+              <p>DJI Mini 3 Pro - 安全概要 (带屏遥控器版) v1.0</p>
+              <p>2022-05-11</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- 第三个 -->
+        <div class="line"> </div>
+<div class="down">
+          <ul>
+            <li class="li1">
+              <p>DJI Mini 3 Pro - 安全概要 (单机) v1.0</p>
+              <p>2022-05-11</p>
+            </li>
+            <li class="li2">
+              <a>
+                <span>pdf</span>
+                <img
+                  src="../assets/download/xz.png"
+                  alt=""
+                  class="download-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+ </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+body{
+   background-color: rgb(247, 247, 247);
+}
+.download {
+  background-color: rgb(247, 247, 247);
+}
+.center {
+  width: 1200px;
+  margin: 0 auto;
+}
+.title h1 {
+  margin-top: 12px;
+  font-size: 32px;
+  line-height: 48px;
+  color: #3b3e40;
+  // padding-bottom: 48px;
+  opacity: 0.85;
+  letter-spacing: -0.03em;
+}
+.app {
+  box-sizing: border-box;
+  padding: 10px 10px 0 10px;
+  background-color: #fff;
+  width: 1200px;
+  margin: 0 auto;
+}
+.app > .center > h4 {
+  font-weight: 600;
+  font-size: 28px;
+  color: #000;
+  line-height: 32px;
+  letter-spacing: -0.03em;
+  padding-left: 0;
+}
+.log-model {
+  padding: 14px 0 48px;
+  display: flex;
+  img {
+    width: 48px;
+    height: 48px;
+  }
+  .right {
+    margin-left: 24px;
+    .r1 {
+      // font-size: 24px;
+      // line-height: 28px;
+      // letter-spacing: -0.03em;
+      font-size: 16px;
+      color: #3b3e40;
+      line-height: 24px;
+    }
+    .r2 {
+      font-size: 16px;
+      color: #6c7073;
+      line-height: 24px;
+    }
+  }
+}
+.app ul {
+  display: flex;
+  justify-content: space-around;
+  padding: 48px 0;
+
+  list-style: none;
+  li {
+    width: 220px;
+    text-align: center;
+    vertical-align: top;
+
+    div:nth-child(2) {
+      color: #3b3e40;
+      font-size: 16px;
+      text-align: center;
+      font-weight: 500;
+      margin-top: 16px;
+    }
+    div:nth-child(3) {
+      padding-top: 8px;
+      font-size: 14px;
+      min-height: 48px;
+      line-height: 24px;
+    }
+    a {
+      font-size: 16px;
+      color: #1897f2;
+      line-height: 24px;
+      cursor: pointer;
+      display: block;
+      text-align: center;
+      p {
+        font-size: 16px;
+        color: #1897f2;
+        line-height: 24px;
+      }
+    }
+  }
+}
+.software {
+  width: 1200px;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 10px 10px 0 10px;
+  box-sizing: border-box;
+
+  
+}
+.software h3 {
+  font-weight: 600;
+  font-size: 28px;
+  color: #000;
+  line-height: 32px;
+  letter-spacing: -0.03em;
+  padding-left: 0;
+}
+.line {
+  border-bottom: 1px solid #f0f1f2;
+}
+.compute {
+  height: 78px;
+  list-style: none;
+  vertical-align: baseline;
+  padding: 32px 24px;
+  display: flex;
+  justify-content: space-between;
+  li {
+    margin-right: 16px;
+    img {
+      width: 64px;
+      height: 64px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-right: 16px;
+    }
+    .com-b {
+      flex: 1;
+      div:nth-child(1) {
+        font-size: 16px;
+        font-weight: 500;
+        color: #3b3e40;
+      }
+      div:nth-child(2) {
+        color: #9fa3a6;
+        font-size: 16px;
+        line-height: 24px;
+      }
+      a {
+        flex-wrap: wrap;
+        margin-top: 8px;
+        display: flex;
+        align-items: center;
+        margin-right: 16px;
+        font-size: 16px;
+        color: #1897f2;
+        line-height: 24px;
+      }
+    }
+  }
+  .download-icon {
+    width: 16px;
+    height: 16px;
+    vertical-align: middle;
+    margin-left: 4px;
+  }
+}
+.mac {
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+}
+.window {
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+}
+
+.down {
+  
+  box-sizing: border-box;
+  padding: 24px;
+  background: #fff;
+  ul {
+      
+    box-sizing: border-box;
+    margin: 0;
+    list-style: none;
+    background: #f7f9fa;
+    padding: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .li1 {
+      
+      width: 774px;
+      display: flex;
+      p:nth-child(1) {
+         line-height: 24px;
+      height: 24px;
+        width: 387px;
+        display: inline-block;
+        font-size: 14px;
+        color: #3b3e40;
+      }
+      p:nth-child(2) {
+         line-height: 24px;
+      height: 24px;
+        width: 387px;
+        text-align: center;
+        display: inline-block;
+        flex: 1;
+        font-size: 14px;
+        color: #3b3e40;
+      }
+    }
+    .li2 {
+     
+      text-align: right;
+      width: 378px;
+      font-size: 14px;
+      color: #3b3e40;
+      a {
+        color: #1897f2;
+        //vertical-align: middle;
+        font-size: 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        display: block;
+        margin-top: 12px;
+        img {
+          width: 16px;
+          height: 16px;
+          vertical-align: middle;
+          margin-left: 4px;
+        }
+      }
+    }
+  }
+}
+
+.tab {
+  width: 1200px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  margin-top: 20px;
+  padding: 20px;
+  background-color: #fff;
+  margin-bottom: 50px;
+   border-bottom: 1px solid #f0f1f2;
+}
+.down2 ul{
+  background-color: #fff;
+  
+}
+</style>

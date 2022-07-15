81c8193f1856a29aff945e0b33e58aa64e026f25
szp <79987419@qq.com>
2022-07-15 20:36:09 +0800

    7/15

--- vue-dj/src/views/Common.vue
+++ vue-dj/src/views/Common.vue
@@ -1,24 +1,25 @@
 <template>
-  <div v-if="data">
+
+  <div v-if="data" class="common">
     <template>
+      <h1>常见问题</h1>
       <el-tabs :tab-position="tabPosition" style="height: 2000px">
+        <h2>产品</h2>
         <el-tab-pane :label="item.c_name" class="pane" v-for="item in data.data" :key="item.c_id">
           <el-collapse  @change="handleChange">
-            <el-collapse-item :title="item.c_title">
+            <el-collapse-item :title="item.c_title.split('$$')[i-1]" v-for="i in item.c_title.split('$$').length" :key="i">
               <div>
-                {{dd}}
+                {{item.c_details.split('$$')[i-1]}}
               </div>
-            </el-collapse-item>
-            
-         
+            </el-collapse-item> 
+
 
           </el-collapse>
         </el-tab-pane>
       </el-tabs>
     </template>
+    <div id="a"></div>
   </div>
-</template>
-    </div>
 </template>
 <script>
 export default {
@@ -26,16 +27,18 @@ export default {
     return {
       tabPosition: "left",
       data:'',
-      dd:''
+      dd:'',
+      c:'jklsajfksaf<br>dfadslfkjaslfk<br>'
     };
   },
   mounted(){
     this.axios.get('http://localhost:3000/v1/common/get').then(res => {
         console.log(res)
-        // this.data = res.data
-        
-        let aa = res.data.data[0].c_title.split()
-        console.log(aa,"AA")
+        this.data = res.data
+        let aa = res.data.data[0].c_details
+        console.log(aa.split('$$'),"AA")
+        let a =document.getElementById('a')
+        a.innerHTML = c
     })
   },
   methods: {
@@ -46,7 +49,38 @@ export default {
 };
 </script>
 <style>
+body{
+  background-color: #f0f1f2;
+}
+
 
 </style>
-<style lang="scss">
+<style>
+.common{
+  width: 1200px;
+  margin: 0 auto;
+}
+.el-tabs__nav-scroll{
+  width: 288px;
+
+}
+ .el-tabs--left .el-tabs__item.is-left{
+    text-align: left;
+    font-size: 16px;
+    padding: 12px 24px;
+  }
+  .el-tabs__item:hover{
+    color: rgba(0,0,0,.85);
+  }
+  .el-tabs__item.is-active{
+    color: rgba(0,0,0,.85);
+  }
+  .el-tabs__nav-scroll{
+    background-color: #fff;
+  }
+  .el-tabs__content{
+    background-color: #fff;
+    padding: 48px 48px 56px;
+  }
+$--color-primary: teal;
 </style>

ccc0e72df254d7e67e07078c2c2d62815542abb6
szz <79987419@qq.com>
2022-07-16 00:30:28 +0800

    7/16

--- vue-dj/src/views/Common.vue
+++ vue-dj/src/views/Common.vue
@@ -1,45 +1,60 @@
 <template>
-
   <div v-if="data" class="common">
     <template>
       <h1>常见问题</h1>
-      <el-tabs :tab-position="tabPosition" style="height: 2000px">
-        <h2>产品</h2>
-        <el-tab-pane :label="item.c_name" class="pane" v-for="item in data.data" :key="item.c_id">
-          <el-collapse  @change="handleChange">
-            <el-collapse-item :title="item.c_title.split('$$')[i-1]" v-for="i in item.c_title.split('$$').length" :key="i">
+      <el-tabs :tab-position="tabPosition">
+        
+        <el-tab-pane
+          :label="item.c_name"
+          class="pane"
+          v-for="item in data.data"
+          :key="item.c_id"
+        >
+          <h2>{{ item.c_name }}</h2>
+          <el-collapse @change="handleChange" id="el">
+            
+            <el-collapse-item
+              :title="item.c_title.split('$$')[i - 1]"
+              v-for="i in item.c_title.split('$$').length"
+              :key="i"
+            >
+            
               <div>
-                {{item.c_details.split('$$')[i-1]}}
+                {{ item.c_details.split("$$")[i - 1] }}
               </div>
-            </el-collapse-item> 
-
-
+              
+            </el-collapse-item>
+            
           </el-collapse>
+          
         </el-tab-pane>
+        
       </el-tabs>
     </template>
+   
     <div id="a"></div>
+  
   </div>
 </template>
+<script>
+</script>
 <script>
 export default {
   data() {
     return {
       tabPosition: "left",
-      data:'',
-      dd:'',
-      c:'jklsajfksaf<br>dfadslfkjaslfk<br>'
+      data: "",
     };
   },
-  mounted(){
-    this.axios.get('http://localhost:3000/v1/common/get').then(res => {
-        console.log(res)
-        this.data = res.data
-        let aa = res.data.data[0].c_details
-        console.log(aa.split('$$'),"AA")
-        let a =document.getElementById('a')
-        a.innerHTML = c
-    })
+  mounted() {
+    this.axios.get("http://localhost:3000/v1/common/get").then((res) => {
+      console.log(res);
+      this.data = res.data;
+      let aa = res.data.data[0].c_details;
+      console.log(aa.split("$$"), "AA");
+      let a = document.getElementById("a");
+      a.innerHTML = c;
+    });
   },
   methods: {
     handleChange(val) {
@@ -48,39 +63,46 @@ export default {
   },
 };
 </script>
+
 <style>
-body{
+body {
   background-color: #f0f1f2;
 }
-
-
 </style>
 <style>
-.common{
+.common {
   width: 1200px;
   margin: 0 auto;
 }
-.el-tabs__nav-scroll{
+.el-tabs__nav-scroll {
   width: 288px;
-
 }
- .el-tabs--left .el-tabs__item.is-left{
-    text-align: left;
-    font-size: 16px;
-    padding: 12px 24px;
-  }
-  .el-tabs__item:hover{
-    color: rgba(0,0,0,.85);
-  }
-  .el-tabs__item.is-active{
-    color: rgba(0,0,0,.85);
-  }
-  .el-tabs__nav-scroll{
-    background-color: #fff;
-  }
-  .el-tabs__content{
-    background-color: #fff;
-    padding: 48px 48px 56px;
-  }
-$--color-primary: teal;
+.el-tabs--left .el-tabs__item.is-left {
+  text-align: left;
+  font-size: 16px;
+  padding: 12px 24px;
+}
+.el-tabs__item:hover {
+  color: rgba(0, 0, 0, 0.85);
+}
+.el-tabs__item.is-active {
+  color: rgba(0, 0, 0, 0.85);
+}
+.el-tabs__nav-scroll {
+  background-color: #fff;
+}
+.el-tabs__content {
+  background-color: #fff;
+  padding: 48px 48px 56px;
+}
+.el-tabs__nav .is-left{
+  height: 100%;
+}
+#a{
+  background-color: aqua;
+  height: 300px;
+} 
+#tab-6{
+  padding-bottom:  79%;
+}
 </style>

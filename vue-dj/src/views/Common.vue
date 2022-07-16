004445e03867babe014e0761d0af10061619b4de
szp <79987419@qq.com>
2022-07-16 12:04:04 +0800

    716

--- vue-dj/src/views/Common.vue
+++ vue-dj/src/views/Common.vue
@@ -1,39 +1,34 @@
+
 <template>
+
   <div v-if="data" class="common">
     <template>
-      <h1>常见问题</h1>
-      <el-tabs :tab-position="tabPosition">
-        
-        <el-tab-pane
-          :label="item.c_name"
-          class="pane"
-          v-for="item in data.data"
-          :key="item.c_id"
-        >
-          <h2>{{ item.c_name }}</h2>
-          <el-collapse @change="handleChange" id="el">
-            
-            <el-collapse-item
-              :title="item.c_title.split('$$')[i - 1]"
-              v-for="i in item.c_title.split('$$').length"
-              :key="i"
-            >
-            
-              <div>
-                {{ item.c_details.split("$$")[i - 1] }}
-              </div>
-              
-            </el-collapse-item>
-            
-          </el-collapse>
-          
-        </el-tab-pane>
-        
-      </el-tabs>
+        <h1>常见问题</h1>
+         <div style="background-color:#fff;position: relative;">
+          <div class="g"></div> <!-- 盖条 -->
+        <el-tabs :tab-position="tabPosition">
+          <el-tab-pane
+            :label="item.c_name"
+            class="pane"
+            v-for="item in data.data"
+            :key="item.c_id"
+          >
+            <h2>{{ item.c_name }}</h2>
+            <el-collapse @change="handleChange" id="el">
+              <el-collapse-item
+                :title="item.c_title.split('$$')[i - 1]"
+                v-for="i in item.c_title.split('$$').length"
+                :key="i"
+              >
+                <div>
+                  {{ item.c_details.split("$$")[i - 1]}}
+                </div>
+              </el-collapse-item>
+            </el-collapse>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
     </template>
-   
-    <div id="a"></div>
-  
   </div>
 </template>
 <script>
@@ -65,6 +60,14 @@ export default {
 </script>
 
 <style>
+.g {
+  background-color: #f0f1f2;;
+  width: 12px;
+  height: 100%;
+  position:absolute;
+  z-index: 3;
+  left: 23%;
+}
 body {
   background-color: #f0f1f2;
 }
@@ -90,19 +93,19 @@ body {
 }
 .el-tabs__nav-scroll {
   background-color: #fff;
+  z-index: -1;
 }
 .el-tabs__content {
   background-color: #fff;
   padding: 48px 48px 56px;
 }
-.el-tabs__nav .is-left{
+.el-tabs__header .is-left {
   height: 100%;
 }
-#a{
-  background-color: aqua;
-  height: 300px;
-} 
-#tab-6{
-  padding-bottom:  79%;
+.is-active{
+  color: #000 !important;
+}
+.el-tabs__item:hover {
+    background: #f0f1f2;
 }
 </style>
